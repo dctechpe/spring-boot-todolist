09d2e4d1333ecf7de6f8d3c83b9d1a07002cc3a3
dcalmape <dcalmape@gmail.com>
2020-06-21 22:16:11 -0500

    Creada entidad usuario

--- src/test/java/pe/dcalma/springboot/todolist/UsuarioTest.java
+++ src/test/java/pe/dcalma/springboot/todolist/UsuarioTest.java
@@ -1,13 +1,28 @@
 package pe.dcalma.springboot.todolist;
 
+import pe.dcalma.springboot.todolist.model.IUsuarioDao;
 import pe.dcalma.springboot.todolist.model.Usuario;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.text.SimpleDateFormat;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class UsuarioTest {
+
+    Logger logger = LoggerFactory.getLogger(UsuarioTest.class);
+
+    @Autowired
+    private IUsuarioDao usuarioDao;
 
     @Test
     public void crearUsuario() throws Exception {
@@ -22,6 +37,15 @@ public class UsuarioTest {
         assertThat(usuario.getNombre()).isEqualTo("Darwin Calma");
         assertThat(usuario.getPassword()).isEqualTo("12345678");
         assertThat(usuario.getFechaNacimiento()).isEqualTo(sdf.parse("1975-05-30"));
+    }
+
+    @Test
+    @Transactional
+    public void crearUsuarioBaseDatos() throws Exception {
+        Usuario usuario = new Usuario("dcalma@gmail.com");
+        usuarioDao.save(usuario);
+        assertThat(usuario.getId()).isNotNull();
+        logger.info("Identificador del usuario: " + usuario.getId());
     }
 
 }
