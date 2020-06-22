9c6fc41a1a5e61966e520316dafe3e1d619543af
dcalmape <dcalmape@gmail.com>
2020-06-21 21:31:52 -0500

    Añadida fecha de nacimiento

--- src/test/java/pe/dcalma/springboot/todolist/UsuarioTest.java
+++ src/test/java/pe/dcalma/springboot/todolist/UsuarioTest.java
@@ -3,6 +3,8 @@ package pe.dcalma.springboot.todolist;
 import pe.dcalma.springboot.todolist.model.Usuario;
 import org.junit.jupiter.api.Test;
 
+import java.text.SimpleDateFormat;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UsuarioTest {
@@ -12,8 +14,14 @@ public class UsuarioTest {
         Usuario usuario = new Usuario("dcalma@gmail.com");
         usuario.setNombre("Darwin Calma");
         usuario.setPassword("12345678");
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        usuario.setFechaNacimiento(sdf.parse("1975-05-30"));
+
         assertThat(usuario.getEmail()).isEqualTo("dcalma@gmail.com");
         assertThat(usuario.getNombre()).isEqualTo("Darwin Calma");
         assertThat(usuario.getPassword()).isEqualTo("12345678");
+        assertThat(usuario.getFechaNacimiento()).isEqualTo(sdf.parse("1975-05-30"));
     }
+
 }
