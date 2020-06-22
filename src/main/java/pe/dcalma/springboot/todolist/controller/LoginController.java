7d19ebbfaedf8e6568cf8ec12a50995f041006e6
dcalmape <dcalmape@gmail.com>
2020-06-22 10:48:09 -0500

    Añadido formulario de login

---
+++ src/main/java/pe/dcalma/springboot/todolist/controller/LoginController.java
@@ -0,0 +1,16 @@
+package pe.dcalma.springboot.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginForm(Map<String, Object> model) {
+        model.put("loginData", new LoginData());
+        return "loginForm";
+    }
+}
---
+++ src/main/java/pe/dcalma/springboot/todolist/controller/LoginData.java
@@ -0,0 +1,22 @@
+package pe.dcalma.springboot.todolist.controller;
+
+public class LoginData {
+    private String eMail;
+    private String password;
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
